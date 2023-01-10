502df375bf38f4118bddc9973ab1910792ebf9a3
im-suryansh <tsuryansh53@gmail.com>
2023-01-11 00:25:42 +0530

    project 5 completed

---
+++ 5 blurry loading/js.js
@@ -0,0 +1,16 @@
+// document.querySelector(".container")
+
+var time = document.getElementsByClassName("timer");
+
+let i = 100;
+
+function Timeout() {
+  if (i > 0) {
+    i--;
+    let calc = i / 10;
+    console.log(i);
+    document.querySelector(".container").style.filter = "blur(" + calc + "px)";
+    document.querySelector(".text").innerText = i + "%";
+  }
+}
+var myInterval = setInterval(Timeout, 20);
