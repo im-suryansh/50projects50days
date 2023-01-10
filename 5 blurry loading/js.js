286fe644f8c37a4c9583758774daf735cae59bb2
im-suryansh <tsuryansh53@gmail.com>
2023-01-11 00:32:34 +0530

    5th edit

--- 5 blurry loading/js.js
+++ 5 blurry loading/js.js
@@ -10,7 +10,9 @@ function Timeout() {
     let calc = i / 10;
     console.log(i);
     document.querySelector(".container").style.filter = "blur(" + calc + "px)";
-    document.querySelector(".text").innerText = i + "%";
+    document.querySelector(".text").innerText = 100-i + "%";
+    document.querySelector(".text").style.opacity = i/100 ;
+
   }
 }
 var myInterval = setInterval(Timeout, 20);
