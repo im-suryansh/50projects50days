be1fd79f1549b8ba36fd08c14b191e3751a682b7
im-suryansh <tsuryansh53@gmail.com>
2023-01-12 22:43:43 +0530

    day 11 done

---
+++ 11 event key codes/index.js
@@ -0,0 +1,15 @@
+let before=document.querySelector(".before")
+let key=document.querySelector(".key")
+let keyccode=document.querySelector(".keyCode")
+let code=document.querySelector(".code")
+let box=document.querySelector(".box")
+
+window.addEventListener('keydown',function(event){
+    before.style.display='none'
+    box.style.display='flex'
+    key.innerText=`' ${event.key}'`
+    code.innerText=`${event.code}`
+    keyccode.innerText=`${event.keyCode}`
+})
+
+    
